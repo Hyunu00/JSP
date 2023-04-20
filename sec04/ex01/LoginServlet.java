9fa425ea696ddfeb40f2542fbed182b893b484a8
Hyunu00 <127381975+Hyunu00@users.noreply.github.com>
2023-04-20 15:18:39 +0900

    Add files via upload

--- sec04/ex01/LoginServlet.java
+++ sec04/ex01/LoginServlet.java
@@ -3,6 +3,8 @@ package sec04.ex01;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,47 +12,51 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-@WebServlet("/Login4")
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/Login2")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doHandle(request, response);
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		System.out.print("init 메서드 호출");
 	}
 
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+		System.out.print("destroy 메서드 호출");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doHandle(request, response);
-	}
-
-	protected void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setCharacterEncoding("utf-8");		
-	    response.setContentType("text/html;charset=utf-8");
-	    PrintWriter out=response.getWriter();
-	    
+		// TODO Auto-generated method stub
+//		request.setCharacterEncoding("utf-8");
+//	    response.setContentType("text/html;charset=utf-8");
+	    PrintWriter out = response.getWriter();
+	    HttpSession session = request.getSession();
 	    String user_id = request.getParameter("user_id");
-	    String user_pwd = request.getParameter("user_pw");
+	    String user_pw = request.getParameter("user_pw");
 	    
-	    MemberVO memberVO = new MemberVO();
-	    memberVO.setId(user_id);
-	    memberVO.setPwd(user_pwd);
-	    
-	    MemberDAO dao = new MemberDAO();
-	    boolean result = dao.isExisted(memberVO);
-	    
-	    if (result) {
-	    	HttpSession session = request.getSession();
-	    	session.setAttribute("isLogon", true);
-	    	session.setAttribute("login.id", user_id);
-	    	session.setAttribute("login.pwd", user_pwd);
-	    	out.print("<html><body>");
-	    	out.print("안녕하세요 " + user_id + "님!!!<br>");
-	    	out.print("<a href='show'>회원정보 보기</a>");
-	    	out.print("</body></html>");
-	    } else {
-	    	out.print("<html><body><center>회원 아이디가 틀립니다.");
-	    	out.print("<a href='login4.html'>다시 로그리인하기</a>");
-	    	out.print("</body></html>");
+	    LoginImpl loginUser = new LoginImpl(user_id,user_pw);
+	    if(session.isNew()) {
+	    	session.setAttribute("loginUser",loginUser);
 	    }
+	    out.print("<head>");
+	    out.print("<script type='text/javascript'>");
+	    out.print("setTimeout('history.go(0);',5000)");
+	    out.print("</script>");
+	    out.print("</head>");
+	    out.print("<html><body>");
+	    out.print("아이디는 " + loginUser.user_id+"<br>");
+	    out.print("총 접속자수는 "+LoginImpl.total_user+"<br>");
+	    out.print("</body></html>");
 	}
-
 }
